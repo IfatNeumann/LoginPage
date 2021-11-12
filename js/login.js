be76c914ab831af7105661c076b778bee5670b84
IfatNeumann <ineumann19@gmail.com>
2021-11-12 21:57:33 +0000

    clean the code

--- js/login.js
+++ js/login.js
@@ -1,34 +1,30 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const usersToPasswords = {
-    "ifat":"qwerty",
-    "rotem":"123456",
-    "chen":"ILoveJustinBieber"
-};
 
-// function getHashedPassword(password) {
-//     for (var i = 0; i < password.length; i++) {
-//         var char = password.charCodeAt(i);
-//         password.replaceAt(i, char+1);
-//     }
-//     return password;
-// }
+const usersToPasswords = {
+    ifat:"qwerty",
+    rotem:"123456",
+    chen:"ILoveJustinBieber"
+};
 
 // When the login button is clicked, the following code is executed
 loginButton.addEventListener("click", (e) => {
+
     // Prevent the default submission of the form
     e.preventDefault();
+
     // Get the values input by the user in the form fields
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-    // console.log(username);
-    // console.log(String.prototype.hashCode(username));
+    
     if (username in usersToPasswords && usersToPasswords[username] === password) {
+
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         window.location.replace("otherpage.html");
     } else {
+        
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
     }
